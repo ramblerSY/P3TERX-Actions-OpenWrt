71971030bb3ced429f2d7178bd0b7698e05adfb0
RamblerSY <younger911@gmail.com>
2023-11-13 11:45:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,9 +20,9 @@
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 
-echo '移除bcm53xx中的其他机型'
-sed -i '421,453d' target/linux/bcm53xx/image/Makefile
-sed -i '140,412d' target/linux/bcm53xx/image/Makefile
-sed -i 's/$(USB3_PACKAGES) k3screenctrl/luci-app-k3screenctrl/g' target/linux/bcm53xx/image/Makefile
+#echo '移除bcm53xx中的其他机型'
+#sed -i '421,453d' target/linux/bcm53xx/image/Makefile
+#sed -i '140,412d' target/linux/bcm53xx/image/Makefile
+#sed -i 's/$(USB3_PACKAGES) k3screenctrl/luci-app-k3screenctrl/g' target/linux/bcm53xx/image/Makefile
 # sed -n '140,146p' target/linux/bcm53xx/image/Makefile
-echo '=========Remove other devices of bcm53xx OK!========='
+#echo '=========Remove other devices of bcm53xx OK!========='
