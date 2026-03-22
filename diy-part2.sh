04072d22ad1338988ed228ff8ce11e69881b3e1a
RamblerSY <younger911@gmail.com>
2026-03-22 13:17:37 +0800

    Update diy-part2.sh
    修改lan ip为192.168.2.1

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,3 +13,11 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 #sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+
+# 1. 修改常规的默认 IP 定义
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+# 2. 修改板级定义的默认网络设置（针对新版源码的关键步骤）
+# 这一步会替换 board.d 文件夹下可能存在的硬编码 IP
+find package/base-files/files/etc/board.d/ -type f -exec sed -i 's/192.168.1.1/192.168.2.1/g' {} +
